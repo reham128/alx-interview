7d0ec8eafb399f379c0751a1c0f21c9446295a36
Reham <r_nour30@yahoo.com>
2024-08-25 09:39:00 +0300

    up

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""a script that reads stdin line by line and computes metrics"""
 import random
 import sys
 from time import sleep
 import datetime
 
 for i in range(10000):
+    """a script that reads stdin line by line and computes metrics"""
     sleep(random.random())
     sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
         random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
