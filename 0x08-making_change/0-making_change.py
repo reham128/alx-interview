3102260b8678e09e2eb3cc6739e1f49fce1b91a8
Reham <r_nour30@yahoo.com>
2024-09-19 16:29:04 +0300

    up

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -14,7 +14,9 @@ def makeChange(coins, total):
         return 0
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
-    for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
     return dp[total] if dp[total] != float('inf') else -1
